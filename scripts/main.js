85d4e7f8337843768c9b03d0008ac41a3411365b
Luis Vivas <43039999+luviv@users.noreply.github.com>
2021-03-11 21:06:17 -0500

    boy and girl form

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,10 @@ window.addEventListener('load', () => {
 
     const button = document.querySelector('.createBtn');
     const genderOptions = document.querySelectorAll('.gender__option');
-    console.log(genderOptions);
+    const formContainer = document.querySelector('#formScreen');
+    const genderContainer = document.querySelector('#genderScreen')
     
+
     genderOptions.forEach(option => {
         option.addEventListener('click', optionClickHandler);
     });
@@ -17,5 +19,28 @@ window.addEventListener('load', () => {
         event.target.classList.add('gender__option--active');
 
         button.disabled = false;
+    }
+
+    button.addEventListener('click', buttonClickHandler);
+    function buttonClickHandler(event) {
+        formContainer.classList.remove('inactive');
+        genderContainer.classList.add('inactive');
+
+        const selectedOption = document.querySelector('.gender__option--active');
+        const isMale = selectedOption.dataset.ismale == 'true';
+        console.log(selectedOption.dataset.ismale);
+
+        const img = formContainer.querySelector('img');
+        const genderText = formContainer.querySelector('h2');
+
+        if(isMale) {
+            formContainer.classList.add('container--reverse');
+            img.setAttribute('src', './assets/boy.png');
+            genderText.innerText = 'BOY';
+        } else {
+            img.setAttribute('src', './assets/girl.png');
+            genderText.innerText = 'GIRL';
+        }
+        button.disabled = true;
     }
 });
